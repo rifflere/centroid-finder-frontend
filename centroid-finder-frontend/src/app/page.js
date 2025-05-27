edd965adb886423e95f19586b72a9d5ea46b4d10
Augy Markham <katiefaithmarkham@gmail.com>
2025-05-27 12:38:43 -0700

    deleted extra unecessary files and added favicon

--- centroid-finder-frontend/src/app/page.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Link from 'next/link';
-
-export default function Home() {
-  return (
-    <div style={{ border: '2px solid', borderColor: 'gold' }}> Main Content </div>
-  );
-}
---
+++ centroid-finder-frontend/src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ border: '2px solid', borderColor: 'gold' }}> 
+      <h1>ART Salamander Solutions -- Main Page</h1>
+    </div>
+  );
+}
