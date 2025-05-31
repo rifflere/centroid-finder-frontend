b34c2d3ec024979ba2b742e8305e1176b966e1cf
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2025-05-30 19:11:25 -0700

    added a small sentence for the video page.

--- centroid-finder-frontend/src/app/videos/page.jsx
+++ centroid-finder-frontend/src/app/videos/page.jsx
@@ -3,6 +3,7 @@ export default function VideoChooserPage({children}){
     return (
         <div style={{ border: '2px solid', borderColor: 'gold' }}>
             <h1>Choose a video: </h1>
+            <p>This page will show the list of videos:</p>
         </div>
     );
 }
