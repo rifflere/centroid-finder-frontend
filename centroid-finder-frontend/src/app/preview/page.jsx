b5317485017677262bd0f512707033b697717d50
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2025-05-30 18:37:19 -0700

    Created a missing page.jsx page for the previews folder

--- centroid-finder-frontend/src/app/preview/page.jsx
+++ centroid-finder-frontend/src/app/preview/page.jsx
@@ -1,7 +1,7 @@
 export default function Page({children}){
     return (
         <div style={{ border: '2px solid', borderColor: 'gold' }}>
-            Filename Page
+            Preview Page
         </div>
     );
 }
