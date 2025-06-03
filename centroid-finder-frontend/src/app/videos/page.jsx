6384e6aa56aac71665e46dbb555df8a62ff3d21b
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-06-03 11:44:10 -0700

    Added get video fetch call to videos page

--- centroid-finder-frontend/src/app/videos/page.jsx
+++ centroid-finder-frontend/src/app/videos/page.jsx
@@ -1,3 +1,21 @@
 export default function Page({children}){
-    return <div style={{ border: '2px solid', borderColor: 'gold' }}> Videos Page </div>;
+    async function getVideos() {
+        const res = await fetch("http://localhost:3000/api/videos");
+        const data = await res.json();
+        
+        videoList = data;
+        console.log(videoList);
+        return data;
+    }
+
+    const videoList = getVideos();
+
+    return (
+    <div style={{ border: '2px solid', borderColor: 'gold' }}>
+        <h1>Videos Page</h1>
+        <ul>
+            // TODO: Render videos here
+        </ul>
+        <p> List of videos above </p>
+    </div>);
 }
