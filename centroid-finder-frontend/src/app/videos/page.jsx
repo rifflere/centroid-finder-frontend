30899d24df6ebd4fdee56e2252bb49ef1fe1f919
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-06-04 15:20:07 -0700

    Cleaned up get call to api/videos

--- centroid-finder-frontend/src/app/videos/page.jsx
+++ centroid-finder-frontend/src/app/videos/page.jsx
@@ -6,16 +6,15 @@ export default function VideoChooserPage({children}){
     // Temporary Data until we get an api route to paste here or whatever we use to retrieve data from Docker
     const sampleVideoData = ['video1.mp4', 'video2.mp4', 'video3.mp4'];
 
-        async function getVideos() {
+    async function getVideos() {
         const res = await fetch("http://localhost:3000/api/videos");
         const data = await res.json();
         
-        videoList = data;
-        console.log(videoList);
+        console.log(data);
         return data;
     }
 
-    const videoList = getVideos();
+    const videosJSON = getVideos();
     
     return (
         <div style={{ border: '2px solid', borderColor: 'gold' }}>
