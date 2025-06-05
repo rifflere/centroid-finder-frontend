ca1ab2416384591fb3a14ef6c257643cd736680a
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-06-05 11:05:23 -0700

    Updated layout to use MUI

--- centroid-finder-frontend/src/app/layout.js
+++ centroid-finder-frontend/src/app/layout.js
@@ -1,6 +1,12 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from 'next/link';
+import Image from 'next/link';
+import { Button, ButtonGroup, Typography } from '@mui/material';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,12 +27,15 @@ export default function Layout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-            <nav style={{ border: '2px solid', borderColor: 'blue' }}>
-              <Link className="nav-link" href="/" style={{padding: 15}}>Home</Link>
-              <Link className="nav-link" href="/videos" style={{padding: 15}}>Videos</Link>
-              <Link className="nav-link" href="/preview/file-123" style={{padding: 15}}>Preview</Link>
-            </nav>
+        <ButtonGroup variant="text" aria-label="Basic button group">
+          <Button href="/">Home</Button>
+          <Button href="/videos">Videos</Button>
+          <Button href="/preview/file-123">Settings</Button>
+        </ButtonGroup>
         <main>{children}</main>
+        <footer style={{position:"absolute", bottom:0}}>
+          <Typography variant="body2" component="body2">ART Salamander Solutions &#169; 2025</Typography>
+        </footer>
       </body>
     </html>
   );
