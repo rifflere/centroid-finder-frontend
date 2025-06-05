194081268c530a739a96a3d55e41e229cbef199d
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2025-06-05 11:12:10 -0700

    Added some text to the main page

--- centroid-finder-frontend/src/app/page.jsx
+++ centroid-finder-frontend/src/app/page.jsx
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
-    <div style={{ border: '2px solid', borderColor: 'gold', backgroundColor: 'lightcyan' }}> 
-      <h1>ART Salamander Solutions -- Main Page</h1>
+    <div> 
+      {/* We'll add styling components here after I (Tyler) type up some instructions and blurbs about the project */}
+      <h2>Welcome to the Salamander Finder!</h2>
+      <br />
+      <h4>
+        This program is designed to help salamander researchers from Ohio State University track and record data of salamanders. 
+        Using computer vision, the system processes video footage to identify and follow the movement of individual salamanders in a controlled environment. 
+        It calculates their centroids and logs positional data into CSV files for further analysis. 
+        The goal is to automate and streamline data collection, enabling researchers to study movement patterns, behavior, and environmental responses more efficiently.
+      </h4>
+
+      
     </div>
   );
 }
