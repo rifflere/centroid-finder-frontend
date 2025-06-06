2d27c9b30d8f129841308dd5628f65352faf32b2
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-06-06 16:36:29 -0700

    Fixed up landing page

--- centroid-finder-frontend/src/app/page.jsx
+++ centroid-finder-frontend/src/app/page.jsx
@@ -1,10 +1,15 @@
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Icon, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material";
 import Image from 'next/image';
+import LooksOneIcon from '@mui/icons-material/LooksOne';
+import LooksTwoIcon from '@mui/icons-material/LooksTwo';
+import Looks3Icon from '@mui/icons-material/Looks3';
+import Looks4Icon from '@mui/icons-material/Looks4';
+import Looks5Icon from '@mui/icons-material/Looks5';
 
 export default function Home() {
   return (
-    <Grid rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 2 }} sx={{ padding:4}}>
-      <Grid size={3}>
+    <Grid container spacing={2} sx={{ padding: 4 }}>
+      <Grid size={3} sx={{ padding: 4 }}>
         <Image src="/icon.ico" width="200" height="200" alt="logo"></Image>
         <br />
         <Typography variant="subtitle1" component="h6">
@@ -13,10 +18,40 @@ export default function Home() {
             (Let's find Salamanders)
         </Typography>
       </Grid>
-      <Grid size={9}>
+      <Grid size={9} sx={{ padding: 4 }}>
           <Typography variant="h4" component="h1">
             Instructions
           </Typography>
+          <List>
+
+            <ListItem>
+              <ListItemIcon>
+                <LooksOneIcon color="primary" />
+              </ListItemIcon>
+              <ListItemText>
+                Step 1
+              </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemIcon>
+                <LooksTwoIcon color="primary" />
+              </ListItemIcon>
+              <ListItemText>
+                Step 2
+              </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemIcon>
+                <Looks3Icon color="primary" />
+              </ListItemIcon>
+              <ListItemText>
+                Step 3
+              </ListItemText>
+            </ListItem>
+
+          </List>
           <Button variant="contained" color="primary" href="/videos">Select a Video</Button>
         <br />
       </Grid>
