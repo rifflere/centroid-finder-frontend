6322d29a25c4edd09cce15d1cd9065255ebc3b49
Augy Markham <katiefaithmarkham@gmail.com>
2025-06-13 14:06:00 -0700

    messing with layout

--- centroid-finder-frontend/src/app/layout.js
+++ centroid-finder-frontend/src/app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from 'next/link';
 import Image from 'next/image';
-import { AppBar, Box, Button, ButtonGroup, IconButton, Toolbar, Typography, Grid} from '@mui/material';
+import { AppBar, Box, Button, ButtonGroup, IconButton, Toolbar, Typography, Grid, Container} from '@mui/material';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,24 +26,18 @@ export default function Layout({ children }) {
             <Box sx={{ flexGrow: 1, marginBottom: 2}}>
               
               <AppBar position="static">
-                <Toolbar>
-                  <Grid container sx={{justifyContent: "center"}}>
-                    <Grid >
-                      <Image src="/icon.ico" width="50" height="50" alt="logo"></Image>
-                    </Grid>
-                    <Grid>
-                      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        <Link href="/">Salamander Spotter</Link>
-                      </Typography>
-                    </Grid>
-                    <Grid>
-                      <em>Inveniamus Ensantinas</em>
-                    </Grid>
-                    <Grid>
-                      <Button variant="text" color="string" href="/about">About</Button>
-                    </Grid>
-                  </Grid>
-                </Toolbar>
+                <Container maxWidth="xl">
+                  <Toolbar disableGutters>
+                    <Image src="/icon.ico" width="50" height="50" alt="logo"></Image>
+                    
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    <Link href="/">Salamander Spotter</Link>
+                    </Typography>
+                    
+                    <Button variant="text" color="string" href="/about">About</Button>
+                  </Toolbar>
+                </Container>
+                
               </AppBar>
             </Box>
 
@@ -58,14 +52,17 @@ export default function Layout({ children }) {
           padding: "1rem 0",
           backgroundColor: "#fff", // optional: give contrast if needed
         }}>
+          <Typography variant='caption'>Inveniamus Ensantinas</Typography>
+
           <Typography variant="body2" component="p">ART Salamander Solutions &#169; 2025</Typography>
           <div id="created-by" style={{ textAlign: "center" }}>
-            <Typography variant="body2" component="p"><i>This project was created by Green River College students of the Cohort 20 Software Development BAS program.</i></Typography>
+            
+            <Typography variant="body2" component="p" color="secondary"  ><i>This project was created by Green River College students of the Cohort 20 Software Development BAS program.</i></Typography>
             <ul id="student-linkedin" style={{ listStyle: "none", padding: 0, margin: 0 }}>
               {/* link your LinkedIn Portfolios inside of the hrefs*/}
-              <Link href="https://www.linkedin.com/in/augy-markham/" target="_blank">Augy Markham</Link>
-              <Link href="https://www.linkedin.com/in/rebecca-riffle/" target="_blank">Rebecca Riffle</Link>
-              <Link href="https://www.linkedin.com/in/dev-tylergilmore/" target="_blank">Tyler Gilmore</Link>
+              <Button href="https://www.linkedin.com/in/augy-markham/" target="_blank">Augy Markham</Button>
+              <Button href="https://www.linkedin.com/in/rebecca-riffle/" target="_blank">Rebecca Riffle</Button>
+              <Button href="https://www.linkedin.com/in/dev-tylergilmore/" target="_blank" > Tyler Gilmore</Button>
             </ul>
           </div>
         </footer>
